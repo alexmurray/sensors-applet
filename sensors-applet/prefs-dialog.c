c2596b4534f612ae890fac1719d4e6e6ef35ef91
Alex Murray <murray.alex@gmail.com>
2010-06-26 08:17:16 +0930

    Set icon name for dialog windows

--- sensors-applet/prefs-dialog.c
+++ sensors-applet/prefs-dialog.c
@@ -420,6 +420,8 @@ void prefs_dialog_open(SensorsApplet *sensors_applet) {
                                                                       GTK_STOCK_CLOSE,
                                                                       GTK_RESPONSE_CLOSE,
                                                                       NULL));
+        gtk_window_set_icon_name(GTK_WINDOW(prefs_dialog->dialog),
+                                 "sensors-applet");
         g_object_set(prefs_dialog->dialog,
                      "border-width", 12,
                      "default-width", 480,
